9b055944da6f9aebf903881e39afaa5a7ac9f894
Shamil Siddique <shamilsdq@live.com>
2024-02-16 10:10:21 +0530

    Updated dev example

--- dev/init.js
+++ dev/init.js
@@ -1,6 +1,21 @@
 import TarkaChat from "../src/main.js";
 import "./style.css"
 
+const UPLOAD_TYPES = ["image/png", "image/jpeg"];
+
+function generateImageThumbnail(file) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const thumbnail = document.createElement("img");
+      thumbnail.src = e.target.result;
+      thumbnail.alt = file.name;
+      resolve(thumbnail);
+    };
+    reader.readAsDataURL(file);
+  });
+}
+
 function getPreChatScreen(onClose) {
   const title = document.createElement("h3");
   title.innerText = "Welcome to a demo prechat screen in TarkaChat";
@@ -16,11 +31,14 @@ function getPreChatScreen(onClose) {
   return container;
 }
 
-async function sendMessage(message) {
+async function sendMessage(message, optionalFiles) {
+  console.log(optionalFiles);
   // Do API calls
   await new Promise((resolve) => setTimeout(resolve, 3000));
   // after getting response
-  return Promise.resolve("Recieved: " + message);
+  return Promise.resolve(
+    "Recieved: " + message + ", Images count: " + optionalFiles?.length ?? 0
+  );
 }
 
 const chat = TarkaChat.init({
@@ -29,6 +47,9 @@ const chat = TarkaChat.init({
   greeting: "Hello. How can I assist you today?",
   themeColor: "#F0DAFB",
   selectorId: "tarkachatbot",
+  enableUpload: true,
+  uploadTypes: UPLOAD_TYPES,
+  generateUploadPreview: generateImageThumbnail,
   preChatRenderer: getPreChatScreen,
   submitHandler: sendMessage,
   expand: true,
